72e6fc2c6c49ddd3d34c2e077bd9fe99e5eee80a
neha kumari <nehakumari199460@gmail.com>
2023-08-28 11:47:59 +0530

    integrated signalr in aspx pages

--- Kibana.Rule.Engine/wwwroot/js/Notification.js
+++ Kibana.Rule.Engine/wwwroot/js/Notification.js
@@ -1,20 +1,40 @@
+"use strict";
 
-       var connection = $.hubConnection("https://localhost:7012");
-        var hubProxy = connection.createHubProxy('/notificationHub'); // Replace 'MyHub' with your hub name
+var connection = new signalR.HubConnectionBuilder().withUrl("https://localhost:7012/notificationHub").build();
 
-    // Define a function to handle incoming messages
-       var userid = document.getElementById("userid").innerHTML
-    hubProxy.on('ReceiveNotification', function (userid, message) {
-        // Handle the received message here
-        console.log(user + ': ' + message);
-        });
+this.connection.start().then(() => {
+    console.log('connection started');
+}).catch(err => console.log(err));
 
-    // Start the connection
-    connection.start()
-    .done(function () {
-        console.log('Connected to SignalR hub');
-            })
-    .fail(function () {
-        console.log('Failed to connect to SignalR hub');
-            });
-   
+this.connection.on('privateMessageMethodName', (data) => {
+    debugger;
+    console.log('private Message:' + data);
+});
+
+//connection.on("ReceiveMessage", function (message) {
+//    var li = document.createElement("li");
+//    document.getElementById("messagesList").appendChild(li);
+
+//    li.textContent = ` user says ${message}`;
+//});
+
+this.connection.on('test',(data) => {
+    console.log(data);
+    this.connection.invoke('SendMessage', 'admin1@gmail.com', data).catch(err => console.log(err));
+    document.getElementById("msglabel").innerHTML = data;
+});
+
+//connection.start().then(function () {
+//    document.getElementById("sendButton").disabled = false;
+//}).catch(function (err) {
+//    return console.error(err.toString());
+//});
+
+//document.getElementById("sendButton").addEventListener("click", function (event) {
+
+//    var message = document.getElementById("messageInput").value;
+//    connection.invoke("SendMessage", message).catch(function (err) {
+//        return console.error(err.toString());
+//    });
+//    event.preventDefault();
+//});
